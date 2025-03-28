f7277e36bbd7f016a9a8c9a22776d094b2a9fa0f
dunteyr <39315568+dunteyr@users.noreply.github.com>
2025-03-28 17:38:05 -0600

    Play scale is working and i added all modes of major

--- src/components/PlayScale.vue
+++ src/components/PlayScale.vue
@@ -2,12 +2,24 @@
 import { ref } from 'vue'
 import { useSoundsStore } from '@/stores/store.js'
 import { storeToRefs } from 'pinia'
+import * as Tone from "tone"
 
 const soundsStore = useSoundsStore()
 const { scaleTypes, notes } = storeToRefs(soundsStore)
 
-const selectedScaleType = ref(scaleTypes[0])
+const selectedScaleType = ref(scaleTypes.value[0].name)
 const selectedScaleNote = ref('c4')
+
+const synth = new Tone.Synth().toDestination();
+
+async function playScale(){
+    await Tone.start()
+    const scaleToPlay = soundsStore.createScale(selectedScaleType.value, selectedScaleNote.value)
+
+    for (let i = 0; i < scaleToPlay.length; i++) {
+        synth.triggerAttackRelease(scaleToPlay[i], '8n', Tone.now() + i * 0.5)
+    }
+}
 </script>
 
 <template>
@@ -29,7 +41,7 @@ const selectedScaleNote = ref('c4')
 
             </select>
         </div>
-        <button type="button">Play Scale </button>
+        <button type="button" @click="playScale">Play Scale</button>
     </div>
     
 
--- src/stores/store.js
+++ src/stores/store.js
@@ -4,8 +4,13 @@ export const useSoundsStore = defineStore('sounds', {
     state: () => ({
         notes: ['a','a#','b','c','c#','d','d#','e','f','f#','g','g#'],
         scaleTypes: [
-            {name: 'Major', formula: [2,2,1,2,2,2,1]},
-            {name: 'Minor', formula: [2,1,2,2,1,2,2]}
+            {name: 'Ionian', formula: [2,2,1,2,2,2,1]},
+            {name: 'Dorian', formula: [2,1,2,2,2,1,2]},
+            {name: 'Phrygian', formula: [1,2,2,2,1,2,2]},
+            {name: 'Lydian', formula: [2,2,2,1,2,2,1]},
+            {name: 'Mixolydian', formula: [2,2,1,2,2,1,2]},
+            {name: 'Aeolian', formula: [2,1,2,2,1,2,2]},
+            {name: 'Locrian', formula: [1,2,2,1,2,2,2]},
         ]
     }),
 
@@ -22,12 +27,43 @@ export const useSoundsStore = defineStore('sounds', {
             }
             return newNotes
         },
-        getAllNotes(){
+        getAllNotes(overflow = 0){
             const allNotes = []
-            for(let i=1; i<6; i++){
+            for(let i=1; i<6 + overflow; i++){
                 allNotes.push(...this.addOctaveNum(i))
             }
             return allNotes
+        },
+        createScale(type, rootNote){
+
+            // get the formula for chosen scale type
+            let scaleFormula = []
+            for (const scaleType of this.scaleTypes) {
+                if (scaleType.name === type){
+                    scaleFormula = [...scaleType.formula]
+                }
+            }
+
+            // get an array of all the notes but starting at rootNote
+            const allNotes = this.getAllNotes(2)            
+            const notes = allNotes.slice(allNotes.indexOf(rootNote))
+            const scaleNotes = []
+
+            // add notes to scaleNotes based on scale formula
+            let stepsFromRoot = 0
+            for (let i=0; i<8; i++){
+                let noteToAdd = null
+                if(i == 0){
+                    noteToAdd = notes[i]
+                }
+                else{
+                    stepsFromRoot += scaleFormula[i-1]
+                    noteToAdd = notes[0 + stepsFromRoot]
+                }
+                scaleNotes.push(noteToAdd)               
+            }
+            return scaleNotes
+
         }
 
     },
